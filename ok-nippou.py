b4591154faa012f23dc90683e072db220904f4e4
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-02-01 21:45:19 +0900

    Update ok-nippou.py

--- ok-nippou.py
+++ ok-nippou.py
@@ -2,16 +2,31 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import json
 
 # Google Sheets API 認証設定
 SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-CREDS_FILE = "serviceAccountKey.json"  # 認証キーのJSONファイル
+SERVICE_ACCOUNT_INFO = {
+     "type": "service_account",
+  "project_id": "meimon-map",
+  "private_key_id": "5663f32b0541766cb4313d40cef2f15b0691a20e",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCadCTvSI0Ze/85\nus//jqmHfsFpiZ+4yiLouq1usif+v70jFi4RLZ1DQbw1yrJOky5nhoG1y4iPpBwv\nQRmie+Y7HE1M8O1Bw6dqLQB8aIOLXchsvuup/fR73LcEe0n6aYYfbrx89IDPURET\nhyTC3GCiYdzcjibfYj3Gap1Sh81jxwOzSfSc5UKvxy7aCuihe8H4Esd5PdBaUbVB\nD+k4r48jfKbcc6XpgFRAbWqZaDjGeBPmXZPVOt0QsdIMdCSpTsat+p5tlVZvo6fx\n6d46JjHih09ftr+WjiGJsZA4vaFKd2AKky98454gKsv9zmG5AKPTZyokTjy5z1XI\nrxdSxS2/AgMBAAECggEADo/yx4i8nPF+7894Ou0VeMvvqmaqY37TacPoBC7R7Ifh\nakR2FXKKiEPTXfL9esEPt/0Lj7tf5cMgUSg/JX2vCFWzyBRTGkc7KpyRlik4ddRi\nyDX7/CwQd/koXEjPgcefOKl1JgbbejB3frLYOXMTvVuiujzA14DouCNnL9fT+pss\natHEwK8OToUHiAgXLbKIjoBXRd/zc4vGGeaBsomBf7XqtCqNBPFtmb4pxyJ5UC+T\npKHNzbjYH4cD+AOor0Gk1+SK3sas11NNXWt67erSNqlhGA/cxqLg5UbdZhb/PPQk\nEHkfjnloTismNK668eaKVt5XT3td4zUH3qXesJMD8QKBgQDPyma4VbfpvQ4u93/K\nReOAYdBVhrUYC1wGJhND3lt4jACW6aHvKjVoPc5EuW5c6BIHqAIwPkBEmEDpNzS8\nnmCkHcg2ZhnyvCLy1wU+jF5qSsOSqz/qYXmPeICflTHGzuhR57To2boIYI+c4ibt\nH5hhPA6mkgvBW8VX1OFxc3c8eQKBgQC+SdPh5ieMLIvBUnaaCX265PtmoL2dBsc2\no3nIe/Y0dErzJpjSZwbeyWptNMYtINuiljZ/aMxdimqM0HUHuE3LVFrk2LnCKb1q\nipi1VKHIRvNEqXyjzSUph/uXO78BuDKUzUPHoK2Xlsc1lANH/mUklBYCpX80NQas\noyYMBZk99wKBgQC6/qjGRs3FmY+EENN90rtTs7Lq5NlgFAjyt50qvJaQu11kckh6\nlP+PGd/g1QdOsMJZBYdCpyLrGCGCP15ESDssNmkRG31Khqjk5UAg8+2btkCeY2KX\nqLTeulD2TCuJgHZuDxktW5MhKtTTGGpzhrV4+7Urjc7qaY4E5t0jXgf18QKBgQCB\nDBZqg6hcUrVwpNkT+83Nmo63+di9jiQ59MGZah/9UMSng4xuXDp3ikbnyrt/TWJG\nL/LDkzHNWhqKZrCHTMFNXGbL/gJ0H9R6VYXcq4mQBjXiYcLKX0yNjs/br0QJCX8c\nFNybnGc9f56Xwko7X9X96YPHxa6vnCprl7Usv/s93wKBgDvYZErASYWDLwjp1AJO\nKf9KKGxmMuHW+lJpIvuunF8CPPimKav9o0cPl5xro/xfh1swQ7Lk2aSKlRb+TLxM\niJ3kmOgj0SVGIPkuRhzxGviHz2rAYS8x/SS9RtVuTr8696CHEYCl/b3p7gtOv7if\nj1EuVRmK3l+jdwyywHmKia62\n-----END PRIVATE KEY-----\n",
+  "client_email": "service-account@meimon-map.iam.gserviceaccount.com",
+  "client_id": "107338856589811318823",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/service-account%40meimon-map.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+CREDS = ServiceAccountCredentials.from_json_keyfile_dict(SERVICE_ACCOUNT_INFO, SCOPE)
+client = gspread.authorize(CREDS)
+
 SHEET_NAME = "日報管理"  # スプレッドシート名
 NOTICE_SHEET_NAME = "お知らせ"  # お知らせ用スプレッドシート名
 
 def get_sheet(sheet_name):
-    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
-    client = gspread.authorize(creds)
     return client.open(sheet_name).sheet1
 
 sheet = get_sheet(SHEET_NAME)
