ecba82bfd9fc9368b28657d6c5c49503ace17568
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-02-20 21:04:36 +0900

    Update loguin.py

--- loguin.py
+++ loguin.py
@@ -1,1 +1,21 @@
 
+import time
+import streamlit as st
+from db_utils import authenticate_user
+
+def login():
+    st.title("🔑 ログイン")
+    employee_code = st.text_input("社員コード")
+    password = st.text_input("パスワード", type="password")
+    login_button = st.button("ログイン")
+
+    if login_button:
+        user = authenticate_user(employee_code, password)
+        if user:
+            st.session_state["user"] = user
+            st.success(f"ようこそ、{user['name']} さん！（{', '.join(user['depart'])}）")
+            time.sleep(1)
+            st.session_state["page"] = "タイムライン"
+            st.experimental_rerun()
+        else:
+            st.error("社員コードまたはパスワードが間違っています。")
