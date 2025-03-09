6a60015a1076d0c469f3c9ed076247e2eba83ef9
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-03-10 06:07:44 +0900

    Update ok-nippou.py

--- ok-nippou.py
+++ ok-nippou.py
@@ -116,7 +116,7 @@ def post_report():
 
      # 選択可能な日付リスト（1週間前～本日）
     today = datetime.today().date()
-    date_options = [(today - datetime.timedelta(days=i)) for i in range(7)]
+    date_options = [(today - timedelta(days=i)) for i in range(7)]
     date_options_formatted = [f"{d.strftime('%Y年%m月%d日 (%a)')}" for d in date_options]
 
     # 実施日の選択（リストから選ぶ）
