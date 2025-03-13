bb43826765f1fce3cd6912d0e0c3802abe83e567
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-03-13 16:03:13 +0900

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -63,9 +63,7 @@ def init_db(keep_existing=True):
         タイトル TEXT,
         内容 TEXT,
         日付 TEXT,
-        既読 INTEGER DEFAULT 0,
-        投稿者 TEXT,  # 誰の投稿か
-        コメント者 TEXT  # 誰がコメントしたか
+        既読 INTEGER DEFAULT 0
     )
     """)
 
