8c36cea517db4c64ab3491ed236a0eaa32309f33
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-03-25 16:19:48 +0900

    Update ok-nippou.py

--- ok-nippou.py
+++ ok-nippou.py
@@ -17,7 +17,7 @@ from db_utils import (
     init_db, authenticate_user, save_report, load_reports, 
     load_notices, mark_notice_as_read, edit_report, delete_report, 
     update_reaction, save_comment, load_commented_reports,
-    save_weekly_schedule_comment, add_comments_column  # 追加
+    save_weekly_schedule_comment, add_comments_column, get_weekly_schedule_for_date  # 追加
 )
 
 # excel_utils.py をインポート
