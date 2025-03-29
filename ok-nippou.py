e1f3d65ea30ee42243dae2f32cad53a567e65245
yukio0302 <pika.pika.yukiyan@gmail.com>
2025-03-30 01:05:59 +0900

    Update ok-nippou.py

--- ok-nippou.py
+++ ok-nippou.py
@@ -14,7 +14,7 @@ def get_current_time():
     return datetime.now() + timedelta(hours=9)  # JSTで現在時刻を取得
 
 # サブコーディングから必要な関数をインポート
-from db_utils import (
+from data.db_utils import (
     init_db, authenticate_user, save_report, load_reports, 
     load_notices, mark_notice_as_read, edit_report, delete_report, 
     update_reaction, save_comment, load_commented_reports,
